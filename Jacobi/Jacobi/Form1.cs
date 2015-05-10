89c4be02ffbc0556354b8b80a9abf757cf1eabac
wopqw <wopqw@yandex.ru>
2015-05-10 22:53:28 +0300

    initial commit

---
+++ Jacobi/Jacobi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jacobi
+{
+    public partial class Jacoby : Form
+    {
+        public Jacoby()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Выберите файл:";
+            openFileDialog1.Filter = "Текстовые файлы|*.txt";
+            openFileDialog1.InitialDirectory = @"";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Encrypt the selected file. I'll do this later. :)
+
+            }   
+        }
+    }
+}
