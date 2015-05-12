6bc22a5ea94816d3c2b57382a4d9e086d1a4b5e7
wopqw <wopqw@yandex.ru>
2015-05-12 13:41:22 +0300

    input output is ok

--- Jacobi/Jacobi/Form1.Designer.cs
+++ Jacobi/Jacobi/Form1.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -44,24 +52,105 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(468, 71);
+            this.button2.Location = new System.Drawing.Point(489, 71);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 42);
+            this.button2.Size = new System.Drawing.Size(124, 42);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Use file";
+            this.button2.Text = "Выбрать";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(589, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите файл, из которого будут считываться данные";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(526, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите размер матрицы, которую нужно считать";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Размер N=";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 42);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Ввести";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Рассчитать";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(473, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 42);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Рассчитать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(484, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 26);
+            this.label5.TabIndex = 9;
             // 
             // Jacoby
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 498);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Jacoby";
             this.Text = "Jacoby method";
+            this.Load += new System.EventHandler(this.Jacoby_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +158,14 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Jacobi/Jacobi/Form1.cs
+++ Jacobi/Jacobi/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Jacobi
 {
     public partial class Jacoby : Form
     {
+        public static int N;
+        public static double[,] matrix;
+        private OpenFileDialog openFileDialog1;
         public Jacoby()
         {
             InitializeComponent();
@@ -24,15 +29,68 @@ namespace Jacobi
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Title = "Выберите файл:";
             openFileDialog1.Filter = "Текстовые файлы|*.txt";
-            openFileDialog1.InitialDirectory = @"";
+            openFileDialog1.InitialDirectory = @"Z:\Documents\Visual Studio 2013\Projects\Jacobi\Jacobi\bin\Debug";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 //Encrypt the selected file. I'll do this later. :)
+            }
+            
+        }
 
-            }   
+        private void Jacoby_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            N = int.Parse(textBox1.Text);
+            matrix = new double[N,N];
+            StreamReader streamReader = File.OpenText(openFileDialog1.FileName);
+            for (int i = 0; i < N; i++)
+            {
+                String text = streamReader.ReadLine();
+                String[] arrayString = text.Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = double.Parse(arrayString[j]);
+                }
+            }
+            streamReader.Close();
+            label5.Text = "Сделано!!";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamWriter streamWriter = new StreamWriter("output.txt");
+            /*for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    streamWriter.Write(matrix[i,j].ToString()+" ");
+                }
+                streamWriter.WriteLine();
+            }*/
+            streamWriter.WriteLine(maxElement(matrix).ToString());
+            streamWriter.Close();
+            Process.Start("notepad.exe", "output.txt");
+        }
+        private double maxElement(double[,] array){
+            double answer = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i != j&&answer<Math.Abs(matrix[i,j]))
+                    {
+                        answer = Math.Abs(matrix[i, j]);
+                    }
+                }
+            }
+                return answer;
         }
     }
 }
